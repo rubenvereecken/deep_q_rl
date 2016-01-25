94f8e4ca528c47f935ae09095451bc1284af9e66
Ruben Vereecken <rubenvereecken@gmail.com>
2016-01-25 21:32:39 +0100

    Changed nips startup script

--- aws/run_nips.sh
+++ aws/run_nips.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 # Prepare code and logging dir along with common parameters
-source common.sh
+# source common.sh
+cp -r /shared/deep_q_rl /root
+cd /root/deep_q_rl
+git pull
+cd deep_q_rl
+
+SAVE_PATH=/data/logs
+mkdir -p $SAVE_PATH
+
+COMMON_PARAMS="--save-path=$SAVE_PATH"
 
 # All defaults will be overwritten by arguments passed to this script
 ./run_nips.py $COMMON_PARAMS $@
