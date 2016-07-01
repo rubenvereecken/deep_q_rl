4b815dbb10b9c364f67123aca149a874b0204ba0
Ruben Vereecken <rubenvereecken@gmail.com>
2016-07-01 23:33:58 +0200

    hydra

--- hydra/run_nips.sh
+++ hydra/run_nips.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -l
 #PBS -l nodes=1:ppn=1
+#PBS -M rvereeck@vub.ac.be
+#PBS -m abe
 
 # Label out directory
 LABEL=${LABEL:=out}
@@ -11,11 +13,11 @@ TIME_STR=`python -c "import time; print time.strftime('%d-%m-%Y_%H-%M-%S')"`
 SAVE_PATH="${WORKDIR}/${LABEL}"
 mkdir -p $SAVE_PATH
 
-COMMON_PARAMS="--save-path=$SAVE_PATH"
+PARAMS="--save-path=$SAVE_PATH -r ${ROM} --network-type=${NETWORK_TYPE} --log_level=DEBUG"
 echo "Saving to $SAVE_PATH"
 
 echo "Running job on $HOST - " `date`
 
 # All defaults will be overwritten by arguments passed to this script
-./deep_q_rl/run_nips.py $COMMON_PARAMS $@
+./deep_q_rl/run_nips.py $PARAMS $@
 
