10a5e698d017715b86fc4b2d7b54aea30aaf5f79
Ruben Vereecken <rubenvereecken@gmail.com>
2016-07-01 23:14:36 +0200

    Hydra script

--- hydra/run_nips.sh
+++ hydra/run_nips.sh
@@ -1,11 +1,14 @@
 #!/bin/bash -l
 #PBS -l nodes=1:ppn=1
-#PBS -l walltime=
-#PBS -o nips.out
 
+# Label out directory
+LABEL=${LABEL:=out}
+
+# Not used atm
 TIME_STR=`python -c "import time; print time.strftime('%d-%m-%Y_%H-%M-%S')"` 
 # SAVE_PATH="${TMPDIR}/out/TIME_STR"
-SAVE_PATH="${WORKDIR}/out"
+
+SAVE_PATH="${WORKDIR}/${LABEL}"
 mkdir -p $SAVE_PATH
 
 COMMON_PARAMS="--save-path=$SAVE_PATH"
@@ -14,5 +17,5 @@ echo "Saving to $SAVE_PATH"
 echo "Running job on $HOST - " `date`
 
 # All defaults will be overwritten by arguments passed to this script
-/run_nips.py $COMMON_PARAMS $@
+./deep_q_rl/run_nips.py $COMMON_PARAMS $@
 
