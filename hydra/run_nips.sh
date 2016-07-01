0c4e0669b7c1e3bc31289f19ebabf8e167819c02
Ruben Vereecken <rubenvereecken@gmail.com>
2016-07-01 23:54:14 +0200

    Hydra save to temp directory on local node

--- hydra/run_nips.sh
+++ hydra/run_nips.sh
@@ -3,21 +3,24 @@
 #PBS -M rvereeck@vub.ac.be
 #PBS -m abe
 
-# Label out directory
-LABEL=${LABEL:=out}
-
-# Not used atm
 TIME_STR=`python -c "import time; print time.strftime('%d-%m-%Y_%H-%M-%S')"` 
-# SAVE_PATH="${TMPDIR}/out/TIME_STR"
 
-SAVE_PATH="${WORKDIR}/${LABEL}"
+# Label out directory
+LABEL=${LABEL}
+if [[ ! -z $LABEL ]]; then
+  SAVE_PATH="${TMPDIR}/${LABEL}-${TIME_STR}"
+else 
+  SAVE_PATH="${TMPDIR}/out-${TIME_STR}"
+fi
+
 mkdir -p $SAVE_PATH
+echo "Saving to $SAVE_PATH"
 
 PARAMS="--save-path=$SAVE_PATH -r ${ROM} --network-type=${NETWORK_TYPE} --log_level=DEBUG"
-echo "Saving to $SAVE_PATH"
 
 echo "Running job on $HOST - " `date`
 
 # All defaults will be overwritten by arguments passed to this script
 ./deep_q_rl/run_nips.py $PARAMS $@
 
+cp -r $SAVE_PATH $WORKDIR
