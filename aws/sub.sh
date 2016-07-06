033b49e2872c49683024d12680378d0c681c1a24
Ruben Vereecken <rubenvereecken@gmail.com>
2016-07-06 12:44:26 +0000

    better sub

--- aws/sub.sh
+++ aws/sub.sh
@@ -1,5 +1,6 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-export JOB_NAME=`python -c "import time;print int(time.time())"`
+export JOB_NAME=`python -c "import time;print str(int(time.time()))[-6:]"`
+export JOB_NAME="job-$JOB_NAME"
 echo "Job ID ${JOB_NAME}"
 qsub -N $JOB_NAME -V run.sh $@
