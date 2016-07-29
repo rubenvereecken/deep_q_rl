e10d3bcb856f3135086271285d97bbf159d4caf6
Ruben Vereecken <rubenvereecken@gmail.com>
2016-07-29 11:38:03 +0200

    Added dtype back in?

--- deep_q_rl/recurrent_agent.py
+++ deep_q_rl/recurrent_agent.py
@@ -3,6 +3,7 @@ import os
 import cPickle
 import time
 import logging
+import theano
 
 import numpy as np
 
@@ -18,7 +19,7 @@ class RecurrentAgent(NeuralAgent):
         super(RecurrentAgent, self).__init__(
                  q_network, epsilon_start, epsilon_min,
                  epsilon_decay, replay_memory_size, replay_start_size,
-                 update_frequency, rng, save_path, profile
+                 update_frequency, rng, save_path, profile, network_params
                 )
         self.network_params = network_params
         logging.debug('Initialized Recurrent Agent')
@@ -29,8 +30,10 @@ class RecurrentAgent(NeuralAgent):
 
         # Reset LSTM state, I think we should start anew each episode
         if self.network_params['network_lstm_reset_on_start']:
-            self.network.lstm.hid.set_value(np.zeros_like(self.network.lstm.hid))
-            self.network.lstm.cell.set_value(np.zeros_like(self.network.lstm.cell))
+            self.network.lstm.hid.set_value(np.zeros_like(self.network.lstm.hid,
+                dtype=theano.config.floatX))
+            self.network.lstm.cell.set_value(np.zeros_like(self.network.lstm.cell,
+                dtype=theano.config.floatX))
 
         return action
 
